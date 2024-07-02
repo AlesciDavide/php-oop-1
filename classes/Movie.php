e5bdf137d06f23992b3899591a24d1d44dc7b24e
AlesciDavide <davi.alesci@Gmail.com>
2024-07-02 14:45:57 +0200

    stampo a pagina l'istanza del film

--- classes/Movie.php
+++ classes/Movie.php
@@ -2,4 +2,20 @@
 
 class Movie{
     
+    public $titolo;
+    public $genere;
+    public $durata;
+
+    public function __construct($titolo, $genere, $durata){
+        $this->titolo = $titolo;
+        $this->genere = $genere;
+        $this->durata = $durata;
+    }
+
+    
+    public function TimeToHours() {
+        $hours = floor($this->durata / 60);
+        $minutes = $this->durata % 60;
+        return "$hours ore e $minutes minuti";
+    }
 }
--- index.php
+++ index.php
@@ -3,6 +3,36 @@
 require_once __DIR__ . '/classes/Movie.php';
 
 
+$aFilm = new Movie("Pirati dei caraibi: la maledizione della prima luna", "Azione", 143);
 
+$bFilm = new Movie("Deadpool & Wolverine", "Azione", 127);
+
+var_dump($Movie);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <article class="article">
+            <h2>
+                Titolo: <?php echo $aFilm->titolo?>
+            </h2>
+            <h3>
+                Genere: <?php echo $aFilm->genere?> 
+            </h3>
+            <h3>
+                durata: <?php echo $aFilm->TimeToHours()?>
+            </h3>
+            
+        </article>
+
+    </section>
+</body>
+</html>
